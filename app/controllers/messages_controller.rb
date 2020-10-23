5deb893957c32a685e9174403f6ebf72a771d1ef
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-10-23 16:02:11 -0500

    plivo request to api working from local host

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,5 @@
+include Plivo
+
 class MessagesController < ApplicationController
     skip_before_action :verify_authenticity_token
 
@@ -7,21 +9,26 @@ class MessagesController < ApplicationController
     end
 
     def create 
-        auth_id = Rails.application.credentials.plivo[:auth_id]
-        auth_token = Rails.application.credentials.plivo[:auth_token]
         message = Message.create(message_params)
-
-        # convert auth_id and auth_token to string?
-        client = RestClient.new(auth_id.to_s, auth_token.to_s)
-        message_created = client.messages.create(
-            '+15125185935',
-            `+#{params[:yourphone]}`,
-            # %w[+15127448789],
-            `+#{params[:content]}`
-        )
-
+        
         if message.valid?
             render json: message
+            auth_id = (Rails.application.credentials.plivo[:auth_id]).to_s
+            auth_token = (Rails.application.credentials.plivo[:auth_token]).to_s
+    
+            puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            # p `#{Rails.application.credentials.plivo[:auth_token]}`
+            # p `#{Rails.application.credentials.plivo[:auth_id]}`
+            puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            # convert auth_id and auth_token to string?
+            client = RestClient.new(auth_id, auth_token)
+            message_created = client.messages.create(
+                '+15125185935',
+                # `+#{params[:yourphone]}`,
+                %w[+15127448789],
+                "hi hi hi hi hi"
+                # `+#{params[:content]}`
+            )
         else
             render json: {errors: message.errors.full_messages}
         end
