311fb90f2ef96b24cf14ae20bfb598a01f146488
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-10-28 12:25:37 -0500

    update regex in production.rb so action cable can access http and https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://guarded-taiga-97709.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://gentle-river-41215.herokuapp.com', /http:\/\/guarded-taiga-97709.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://gentle-river-41215.herokuapp.com', /http:\/\/https://gentle-river-41215.herokuapp.com.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+
   
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
