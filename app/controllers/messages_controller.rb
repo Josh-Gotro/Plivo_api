b8f631a2bb936c3d44937a424a02ed80070ac82f
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-10-29 06:04:28 -0500

    deployed MVP

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
     end
 
     def send_sms
+        # Create outgoing message from client request. 
         message = Message.new(
             MessageUUID: "",
             To: message_params[:To], 
@@ -19,17 +20,21 @@ class MessagesController < ApplicationController
             From: message_params[:From], 
             isoutgoing: message_params[:isoutgoing])
 
+        # If no errors, Post message to Plivo API.
         if message.valid?
             message_created = CLIENT.messages.create(
                 message_params[:From], 
                 [message_params[:To]], 
                 message_params[:Text])
+
+        # Attach returned MessagueUUID to local record
             message.update(MessageUUID: message_created.message_uuid[0])
             render json: message
         end 
     end
     
     def accept_sms
+        # Accept incoming message and persist to local API
         message = Message.create(
             MessageUUID: message_params[:MessageUUID],
             Text: message_params[:Text], 
@@ -37,18 +42,21 @@ class MessagesController < ApplicationController
             To: params[:To], 
             isoutgoing: false)
 
+        # If no errors occur in new message, broadcast to React client.  
         if message.valid?
             ActionCable.server.broadcast 'messages_channel', message
         end
     end
 
     def log_sms
+        # Request SMS history for range: <= & >=  dates provided.
         response = CLIENT.messages.list(
             message_time__lte: message_params[:lte],
             message_time__gte: message_params[:gte],
             limit: 20,
             offset: 0,
         )
+        # map over returned records and search local records for SMS that match MessageUUID
         uuids = response[:objects].map {|msg| msg.message_uuid}
         messages = uuids.map do |uuid| 
             # binding.pry
@@ -58,8 +66,26 @@ class MessagesController < ApplicationController
     end
 
 private
+        # Protect attributes from end-user assignment. 
+        # Action Controller parameters forbidden in Active Model mass assignment until explicitly enumerated.
     def message_params
-        # params.require(:message).permit(:content, :myphone, :yourphone, :isoutgoing)
-            params.permit(:message_time__gte, :message_time__lte, :gte, :lte, :content, :isoutgoing, :From, :MessageIntent, :MessageUUID, :PowerpackUUID, :Text, :To, :TotalAmount, :TotalRate, :Type, :Units)
+            params.permit(
+                :message_time__gte, 
+                :message_time__lte, 
+                :gte, 
+                :lte, 
+                :content, 
+                :isoutgoing, 
+                :From, 
+                :MessageIntent, 
+                :MessageUUID, 
+                :PowerpackUUID, 
+                :Text, 
+                :To, 
+                :TotalAmount, 
+                :TotalRate, 
+                :Type, 
+                :Units
+            )
     end
 end
