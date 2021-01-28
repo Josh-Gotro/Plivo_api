3036577581dc45b449677840bf9cbf4cf0dd882d
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-01-27 20:02:35 -0600

    remove sinatra

--- app/controllers/speak.rb
+++ app/controllers/speak.rb
@@ -1,18 +1,18 @@
-require 'rubygems'
-require 'sinatra'
-require 'plivo'
-include Plivo
+# require 'rubygems'
+# require 'sinatra'
+# require 'plivo'
+# include Plivo
 
-# Set te caller ID using Dial XML
+# # Set te caller ID using Dial XML
 
-get '/receive_call' do
-    response = Response.new
+# get '/receive_call' do
+#     response = Response.new
 
-    speak_body = 'Hello, you just received your first call'
-    response.addSpeak(speak_body)
-    xml = PlivoXML.new(response)   
+#     speak_body = 'Hello, you just received your first call'
+#     response.addSpeak(speak_body)
+#     xml = PlivoXML.new(response)   
 
-    puts xml.to_xml() # Prints the XML
-    content_type "application/xml"
-    return xml.to_s() # Returns the XML
-end
+#     puts xml.to_xml() # Prints the XML
+#     content_type "application/xml"
+#     return xml.to_s() # Returns the XML
+# end
