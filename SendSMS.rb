1c8991dc72f85e0c1231e5d912b108ccf5f76efd
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-01-27 19:54:45 -0600

    remove practice code

--- SendSMS.rb
+++ SendSMS.rb
@@ -1,19 +1,19 @@
-require 'rubygems'
-require 'plivo'
+# require 'rubygems'
+# require 'plivo'
 
-include Plivo
+# include Plivo
 
-# puts Rails.application.credentials.plivo[:auth_id]
-# puts Rails.application.credentials.plivo[:auth_token]
+# # puts Rails.application.credentials.plivo[:auth_id]
+# # puts Rails.application.credentials.plivo[:auth_token]
 
-# client = RestClient.new("xxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxx")
-# client = RestClient.new(`#{auth}`, `#{tok}`)
+# # client = RestClient.new("xxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxx")
+# # client = RestClient.new(`#{auth}`, `#{tok}`)
 
-message_created = client.messages.create(
-  '+15125185935',
-  %w[+15127448789],
-  'Test using tight '
-)
+# message_created = client.messages.create(
+#   '+15125185935',
+#   %w[+15127448789],
+#   'Test using tight '
+# )
 
-# Rails.application.credentials.plivo[:auth_id]
-# Rails.application.credentials.plivo[:auth_token]
+# # Rails.application.credentials.plivo[:auth_id]
+# # Rails.application.credentials.plivo[:auth_token]
